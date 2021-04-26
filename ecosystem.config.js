eed89060d32f1c272c2de7737a2fd7200201c1b8
Luciano Tassara <lucianotassara@gmail.com>
2021-04-26 01:20:35 -0300

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps : [{
+      name: 'wa-bot',
+      script: './index.js',
+  
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      args: '--update-env',
+      node_args: '',
+      instances: 1,
+      autorestart: false,
+      watch: false,
+      exec_mode: 'fork',
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'desa'
+      },
+      env_production: {
+        NODE_ENV: 'prod'
+      }
+    }]
+  };
