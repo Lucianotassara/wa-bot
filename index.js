f19b12241b61ab2720a0780e43b8bd5a717c6270
Luciano Tassara <lucianotassara@gmail.com>
2021-06-17 18:45:07 -0300

    saving contacts to mongodb

--- index.js
+++ index.js
@@ -103,7 +103,44 @@ let MessageSchema = new Schema({
     }
 });
 
+let ContactIdSchema = mongoose.Schema({
+    server: {type: String},
+    user: {type: String},
+    _serialized: {type: String}
+    
+});
+
+let ContactSchema = mongoose.Schema({
+    id: {
+        type: ContactIdSchema,
+        required: true
+    },
+    number: {type: String},
+    isBusiness: {type: Boolean},
+    isEnterprise: {type: Boolean},
+    labels: [],
+    name: {type: String},
+    pushname: {type: String},
+    sectionHeader: {type: String},
+    shortName: {type: String},
+    statusMute: {type: Boolean},
+    type: {type: String},
+    verifiedLevel: {type: String},
+    verifiedName: {type: String},
+    isMe: {type: Boolean},
+    isUser: {type: Boolean},
+    isGroup: {type: Boolean},
+    isWAContact: {type: Boolean},
+    isMyContact: {type: Boolean},
+    isBlocked: {type: Boolean}
+      
+});
+
+
+    
+
 const MessageModel = mongoose.model("Message", MessageSchema);
+const ContactModel = mongoose.model("Contact", ContactSchema);
 /*************************************************************************** */
 
 
@@ -240,7 +277,12 @@ client.on('message', async msg => {
                             `   Numero: ${contact.number}\n` +
                             `   Pushname: ${contact.pushname}\n` +
                             `   ShortName: ${contact.shortname}`);
-                                console.log(`Showing contact: `,contact);
+                            console.log(`Showing contact: `,contact);
+                                
+                            //save contact on mongodb:
+                            ContactModel.findOneAndUpdate({"id._serialized": contact.id._serialized}, contact, {upsert: true}, function (err, r) {
+                                if(err) console.log('error: ',err)
+                            });          
                         }else{
                             // msg.reply(`*Contacto no agendado...${number}*`)
                         }
